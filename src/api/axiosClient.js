3be63baf930b59ada154cd00f9ef7004f567d7bc
TienLocCao <caotienloc0310@gmail.com>
2021-08-19 08:22:32 +0700

    deploy

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const MainApi = axios.create({
-baseURL: process.env.VUE_APP_API_URL,
+baseURL: 'https://api.themoviedb.org',
   withCredentials: false,
   headers: {
     Accept: "application/json",
--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -2,7 +2,8 @@ import axiosClient from "./axiosClient";
 
 const movieApi = {
   search: (value) => {
-    const url = `/3/search/movie?api_key=${process.env.VUE_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${value}`;
+    // const url = `/3/search/movie?api_key=${process.env.VUE_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${value}`;
+    const url = `/3/search/movie?api_key=3ceee4a2e8da7a79f7d0bf79c7a3bc6f&language=en-US&page=1&include_adult=false&query=${value}`;
     return axiosClient.get(url);
   }
 }
